d49dfb2e27c317abaf75f8ab5ebbe3754c314d94
hariprasanth-jcode <hari.mba000@gmail.com>
2026-03-02 12:06:19 +0530

    mvc

---
+++ springmvcboot/src/main/java/com/capgemini/springmvcboot/DemoController.java
@@ -0,0 +1,67 @@
+package com.capgemini.springmvcboot;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class DemoController {
+	
+	@Autowired
+	UsersJpaRepository jpa;
+	
+	@GetMapping("/hello")
+	public String getHi() {
+		return "welcome";
+	}
+	
+	@GetMapping("/register")
+	public String createAccount() {
+		return "register";
+	}
+	
+	@PostMapping("/create-account")
+	public String register(@ModelAttribute Users users) {
+		
+		jpa.save(users);
+		
+		return "sucess";
+		
+	}
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+	@PostMapping("/logincheck")
+	public String logincheck(HttpServletRequest request) {
+		
+		String email=request.getParameter("email");
+		String password=request.getParameter("password");
+		
+		Users user = jpa.findByEmailAndPassword(email, password);
+		if(user!=null) {
+			return "loginsucess";
+		}else {
+			return "redirect:/login";
+		}
+	}
+	
+	@GetMapping("/hi")
+	public ModelAndView sendData() {
+		ModelAndView m=new ModelAndView();
+		List<String> names = List.of("Miller","Allen","Smith");
+		m.addObject("msg",names);
+		m.setViewName("abc");
+		return m;
+		
+	}
+
+}
---
+++ springmvcboot/src/main/webapp/WEB-INF/view/loginsucess.jsp
@@ -0,0 +1,9 @@
+<html>
+
+<head>Login success</head>
+<body>
+
+   Welcome to Spring boot
+
+</body>
+</html>
